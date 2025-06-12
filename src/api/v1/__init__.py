b7fd3b08ae81a814d358dc190a46fbccd4bed421
Flavien <you@example.com>
2025-06-12 17:44:30 +0500

    remove src/

--- src/api/v1/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from .listing import router as listing_router
-
-router = APIRouter()
-
-router.include_router(listing_router, prefix="/listings", tags=["Listings"])
